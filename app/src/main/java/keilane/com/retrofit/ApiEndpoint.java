80c4f0701eecc347b579fa13b699f8187fd1d8e0
Keilane Rizza <keilanerizza@gmail.com>
2018-09-03 18:08:43 -0300

    Implementacao da chamada do titulo do Post

--- app/src/main/java/keilane/com/retrofit/ApiEndpoint.java
+++ app/src/main/java/keilane/com/retrofit/ApiEndpoint.java
@@ -7,6 +7,6 @@ import retrofit2.http.Path;
 public interface ApiEndpoint {
 
     @GET("posts/{id}")
-    Call<Post> obterPost(@Path("id") int userID);
+    Call<Post> obterPost(@Path("id") String userID);
 
 }
--- app/src/main/java/keilane/com/retrofit/MainActivity.java
+++ app/src/main/java/keilane/com/retrofit/MainActivity.java
@@ -3,9 +3,15 @@ package keilane.com.retrofit;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.List;
 
 import keilane.com.retrofit.dominio.Post;
 import retrofit2.Call;
@@ -15,13 +21,53 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.POST;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnKeyListener {
+
+    private EditText entrada;
+    private TextView saida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preparacaoInicial();
 
+    }
+
+    private void preparacaoInicial() {
+        entrada = (EditText) findViewById(R.id.idPost);
+        entrada.setOnKeyListener(this);
+
+        saida = (TextView) findViewById(R.id.titlePost);
+        saida.setOnKeyListener(this);
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent event) {
+        if (i == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
+            Call<Post> call = getPostCall();
+
+            call.enqueue(new Callback<Post>() { //chamada assíncrona
+
+                public void onResponse(Call<Post> call, Response<Post> response) {
+                    int statusCode = response.code();
+                    Post post = response.body();
+
+                    saida.setText(post.getTitle());
+                    Log.i("title : " + post.getTitle(), toString());
+                }
+
+                public void onFailure(Call<Post> call, Throwable t) {
+                    // Log error here since request failed
+                    Log.i("teste", t.toString());
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
+    private Call<Post> getPostCall() {
         Gson gson = new GsonBuilder()
 
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
@@ -34,20 +80,9 @@ public class MainActivity extends Activity {
                 .build();
 
         ApiEndpoint apiService = retrofit.create(ApiEndpoint.class);
-        Call<Post> call = apiService.obterPost(1);
-        
-        call.enqueue(new Callback<Post>() { //chamada assíncrona
-
-            public void onResponse(Call<Post> call, Response<Post> response) {
-                int statusCode = response.code();
-                Post post = response.body();
-
-            }
-
-            public void onFailure(Call<Post> call, Throwable t) {
-            // Log error here since request failed
-                Log.i("teste", t.toString());
-            }
-        });
+        return apiService.obterPost(entrada.getText().toString());
     }
+
+
+
 }
